8907ea709f61b863cb7b14dea558d689d21f00db
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-05 09:23:35 +0300

    Create the middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,49 @@
 package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
+)
+
+var signingKey string
+
+func getSigningKey() string {
+	if err := godotenv.Load("../.env"); err != nil {
+		fmt.Println(err.Error())
+		return fmt.Errorf("%s", err).Error()
+	} else {
+		key := os.Getenv("SIGNING_KEY")
+		fmt.Println(key)
+		return key
+	}
+}
+
+func authenticate(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+        if r.Header["Token"] != nil {
+
+            token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+                if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+                    return nil, fmt.Errorf("There was an error")
+                }
+                return signingKey, nil
+            })
+
+            if err != nil {
+                fmt.Fprintf(w, err.Error())
+            }
+
+            if token.Valid {
+                endpoint(w, r)
+            }
+        } else {
+
+            fmt.Fprintf(w, "Not Authorized")
+        }
+    })
+}
