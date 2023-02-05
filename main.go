b51c26af1bbcd9a7cd68d03ae4c4d0bacf2164bd
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-05 08:58:18 +0300

    Add the landing endpoint

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+// public endpoint
+func landing(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to this public endpoint"))
+}
 
 func main() {
 	fmt.Println("Hello friend")
