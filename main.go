f6e0e5f3ee3e2dd76f032e1681466b3492413592
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-05 08:59:35 +0300

    Declare the login endpoint

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func landing(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome to this public endpoint"))
 }
 
+// endpoint to log in
+func login(w http.ResponseWriter, r *http.Request) {
+	
+}
+
 func main() {
 	fmt.Println("Hello friend")
 }
